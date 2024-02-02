e1d472ca546fdb59394bdd2fa1d5001b1729335b
bsquiroz <bemq1605@gmail.com>
2024-02-02 11:57:04 -0500

    add real video explanation

--- src/components/userManual/index.tsx
+++ src/components/userManual/index.tsx
@@ -41,9 +41,8 @@ export const UserManual = () => {
                     <iframe
                         width="100%"
                         height="100%"
-                        src="https://www.youtube.com/embed/MbUDckATH0Q?si=NJoVF32OzxA-8-4_"
+                        src="https://www.youtube.com/embed/-QvctHMv6VI?si=RGxtKKN4FZIwaZDc"
                         title="YouTube video player"
-                        frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         allowFullScreen
                     ></iframe>
